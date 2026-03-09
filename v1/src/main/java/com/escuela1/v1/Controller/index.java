068daede2098cea3cfeeee58db1d2681e7431fa3
Marina Marin <marina.marin75.mm@gmail.com>
2026-03-09 18:34:47 -0500

    Actualizaciones

--- v1/src/main/java/com/escuela1/v1/Controller/index.java
+++ v1/src/main/java/com/escuela1/v1/Controller/index.java
@@ -3,13 +3,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
+
+
 @Controller
 @RequestMapping("/")
 
+
 public class index {
 
-    @GetMapping("index")
+    @GetMapping({"", "/", "index"})
     public String index(){
         return "index";
-    }   
+    }  
+    
+    @GetMapping("/mision")
+    public String mision(){
+        return "mision";
+    }
+
+    @GetMapping("/vision")
+    public String vision(){
+        return "vision";
+    }
+
+    @GetMapping("/historia")
+    public String historia(){
+        return "historia";
+    }
+    
+    
 }
